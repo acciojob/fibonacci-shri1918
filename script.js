c1fe9294af6915ad1bb4843d7eda1112aa677b23
Shridhar Savant <93203347+shri1918@users.noreply.github.com>
2023-07-12 19:42:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 function fibonacci(num) {
-// your code here
-	  if (num <= 0) {
+  if (num <= 0) {
     return 0;
-  } else if (num === 1) {
-    return 1;
   } else {
     let fibList = [0, 1];
     for (let i = 2; i <= num; i++) {
