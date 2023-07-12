36db2108774f4886667747831cf8c8d10f1e3dd2
Shridhar Savant <93203347+shri1918@users.noreply.github.com>
2023-07-12 19:31:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function fibonacci(num) {
 // your code here
+	if (num <= 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  } else {
+    let fibList = [0, 1];
+    for (let i = 2; i <= num; i++) {
+      fibList.push(fibList[i - 1] + fibList[i - 2]);
+    }
+    return fibList[num];
+  }
 }
 
 module.exports = fibonacci;
